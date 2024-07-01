e5d9875d194e4487969abf5ca9c37ecd05e3be7e
Nur <xlr8nur@protonmail.com>
2024-07-01 17:21:43 +0300

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -518,7 +518,7 @@ export default function Home() {
       </div>
 
       <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
+        Made with &#10084; by xlr8nur
       </footer>
     </div>
   );
